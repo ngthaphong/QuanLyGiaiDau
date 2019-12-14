2e8226656aa0cccc41f02302e276b6df31749250
khatag <tangnhutkha209@gmail.com>
2019-12-14 16:00:43 +0700

    update 15:50

--- QuanLyGiaiDau/QuanLyGiaiDau/Form1.Designer.cs
+++ QuanLyGiaiDau/QuanLyGiaiDau/Form1.Designer.cs
@@ -28,12 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Location = new System.Drawing.Point(31, 148);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(75, 23);
+            this.btnDangNhap.TabIndex = 0;
+            this.btnDangNhap.Text = "Đăng Nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(11, 125);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(56, 13);
+            this.lblPass.TabIndex = 1;
+            this.lblPass.Text = "Password:";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(97, 90);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(199, 20);
+            this.txtUser.TabIndex = 2;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(11, 93);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(58, 13);
+            this.lblUser.TabIndex = 3;
+            this.lblUser.Text = "Username:";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(97, 122);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(199, 20);
+            this.txtPass.TabIndex = 2;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(167, 148);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 0;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(314, 192);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.lblPass);
+            this.Name = "Form1";
+            this.Text = "DMM KHA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.TextBox txtPass;
     }
 }
 
--- QuanLyGiaiDau/QuanLyGiaiDau/Form1.cs
+++ QuanLyGiaiDau/QuanLyGiaiDau/Form1.cs
@@ -16,5 +16,15 @@ namespace QuanLyGiaiDau
         {
             InitializeComponent();
         }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
